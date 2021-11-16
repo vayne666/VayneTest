22fe32824f7d9a0ba34781976c7d6faec09efafe
vayne666 <vayne666@qq.com>
2021-11-16 17:39:46 +0800

    update

---
+++ Assets/Async/Async.cs
@@ -0,0 +1,228 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Runtime.CompilerServices;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using UnityEngine;
+//using UnityEngine.Networking;
+
+//public class Async : MonoBehaviour {
+
+
+//    // Use this for initialization
+//    async void Start() {
+//        //Debug.Log(Thread.CurrentThread.ManagedThreadId);
+//        Debug.Log(Time.realtimeSinceStartup);
+//        await Task.Delay(1000);
+//        Debug.Log(Time.realtimeSinceStartup);
+//        Debug.Log("============================");
+//        await TaskManger.Instance.Delay(2);
+//        Debug.Log(Time.realtimeSinceStartup);
+//    }
+
+
+
+//    private void Update() {
+//        TaskManger.Instance.Update();
+//    }
+
+
+//    private DelayTask Delay(int time) {
+//        var task = new DelayTask(2);
+
+
+//        return task;
+//    }
+
+//    IEnumerator Start1() {
+//        yield return new WaitForSeconds(1);
+//    }
+
+
+//    public IEnumerator Get(string url) {
+//        UnityWebRequest request = UnityWebRequest.Get("");
+//        yield return request.SendWebRequest();
+//    }
+
+
+
+//}
+
+
+
+//public class TaskManger {
+
+//    private static TaskManger _instance;
+
+//    public static TaskManger Instance {
+//        get {
+//            if (_instance==null) {
+//                _instance=new TaskManger();
+//            }
+//            return _instance;
+//        }
+//    }
+
+//    List<CustomTask> tasks = new List<CustomTask>();
+
+//    public void Update() {
+//        if (tasks.Count<1) return;
+//        for (int i = 0; i<tasks.Count; i++) {
+//            tasks[i].Update();
+//        }
+//        RemoveCompItem();
+//    }
+
+//    private void RemoveItem(CustomTask item) {
+//        tasks.Remove(item);
+//    }
+
+//    private void RemoveCompItem() {
+//        for (int i = tasks.Count-1; i>=0; i--) {
+//            if (tasks[i].IsComplete) {
+//                tasks.RemoveAt(i);
+//            }
+//        }
+//    }
+
+//    public DelayTask Delay(float time) {
+//        var task = new DelayTask(time);
+//        tasks.Add(task);
+//        return task;
+//    }
+
+//    public void RemoveTask(DelayTask t) {
+//        for (int i = 0; i<tasks.Count; i++) {
+//            if (tasks[i]==t) {
+//                tasks.RemoveAt(i);
+//                return;
+//            }
+//        }
+//    }
+
+
+//}
+
+//public abstract class CustomTask {
+//    private bool isComp;
+//    public abstract bool IsComplete { get; set; }
+
+//    public abstract void Update();
+//    public abstract void Complete();
+//    public abstract void StartTask();
+//    public abstract void EndTask();
+//    public abstract CustomAwaiter GetAwaiter();
+//}
+
+//public abstract class CustomTask<T> : CustomTask {
+//    private T result;
+//    public T Result { get; }
+//}
+
+
+//public class DelayTask : CustomTask {
+
+//    private CustomAwaiter awaiter;
+//    private float time;
+//    private float curTime = 0;
+//    public override bool IsComplete { get; set; }
+
+//    public override CustomAwaiter GetAwaiter() {
+//        return awaiter;
+//    }
+//    public DelayTask(float time) {
+//        awaiter=new CustomAwaiter();
+//        this.time=time;
+//    }
+
+//    public override void Update() {
+//        curTime+=Time.deltaTime;
+//        if (curTime>=time) {
+//            Complete();
+//        }
+//    }
+
+//    public override void Complete() {
+//        awaiter.Complete();
+//        IsComplete=true;
+
+//    }
+
+//    public override void StartTask() {
+//        curTime=0;
+//    }
+
+//    public override void EndTask() {
+
+//    }
+
+//}
+
+//public class HttpRsp {
+//    public int errorCode;
+//    public string data;
+//}
+
+
+//public class CustomAwaiter : ICriticalNotifyCompletion {
+
+//    public CustomTask task;
+
+//    public bool IsCompleted { get; private set; }
+
+//    private Action comp;
+//    //private 
+
+//    public void OnCompleted(Action continuation) {
+//        if (IsCompleted) {
+//            continuation?.Invoke();
+//        } else {
+//            comp+=continuation;
+//        }
+
+//    }
+
+//    public void UnsafeOnCompleted(Action continuation) {
+//        if (IsCompleted) {
+//            continuation?.Invoke();
+//        } else {
+//            comp+=continuation;
+
+//        }
+
+//    }
+
+//    public void Complete() {
+//        IsCompleted=true;
+//        comp?.Invoke();
+//        comp=null;
+
+//    }
+
+//    public void GetResult() {
+
+//    }
+//}
+
+
+//public class CustomAwaiter<T> : ICriticalNotifyCompletion {
+
+//    public CustomTask<T> task;
+
+//    public CustomAwaiter(CustomTask<T> task) {
+//        this.task=task;
+//    }
+
+//    public T GetResult() {
+//        return task.Result;
+//    }
+
+//    public void OnCompleted(Action continuation) {
+
+//    }
+
+//    public void UnsafeOnCompleted(Action continuation) {
+
+//    }
+//}
---
+++ Assets/Async/VTask.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+
+
+public class VTask : MonoBehaviour {
+
+    static List<CustonAwaiter> custonAwaiters = new List<CustonAwaiter>();
+
+
+    private void Awake() {
+
+    }
+
+    bool isComp = false;
+    private void Update() {
+        foreach (var item in custonAwaiters) {
+            var comp = item.Update();
+            if (!isComp) isComp=comp;
+        }
+        if (isComp) {
+            for (int i = custonAwaiters.Count; i>0; i--) {
+                if (custonAwaiters[i].IsCompleted) {
+                    custonAwaiters.RemoveAt(i);
+                }
+            }
+        }
+    }
+
+    public static CustonAwaiter Delay(float time) {
+        var task = new DelayTask(time);
+        custonAwaiters.Add(task);
+        return task;
+    }
+
+}
+
+
+
+public class DelayTask : CustonAwaiter {
+
+    private float time;
+    private float curTime;
+    public DelayTask(float time) {
+        this.time=time;
+    }
+
+    public override bool Update() {
+        curTime+=Time.deltaTime;
+        if (curTime>=time) {
+            this.SetResult();
+            return true;
+        }
+        return false;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+public interface IAwaiter : ICriticalNotifyCompletion {
+    bool IsCompleted { get; }
+    void Update();
+}
+
+public class VAwaiter : ICriticalNotifyCompletion {
+
+    protected bool isCompleted;
+    public bool IsCompleted => isCompleted;
+    protected Action comp;
+    public void OnCompleted(Action continuation) {
+        if (IsCompleted) {
+            continuation?.Invoke();
+        } else {
+            comp+=continuation;
+        }
+    }
+
+    public void UnsafeOnCompleted(Action continuation) {
+        if (IsCompleted) {
+            continuation?.Invoke();
+        } else {
+            comp+=continuation;
+
+        }
+
+    }
+
+    public virtual bool Update() {
+        return false;
+    }
+}
+
+
+public class CustonAwaiter : VAwaiter {
+
+    public void Complete() {
+        comp?.Invoke();
+        comp=null;
+
+    }
+
+    public CustonAwaiter GetAwaiter() {
+        return this;
+    }
+
+    public void SetResult() {
+        isCompleted=true;
+        comp?.Invoke();
+    }
+
+    public void GetResult() {
+
+    }
+}
+
+public class CustonAwaiter<T> : VAwaiter {
+    T result;
+    public CustonAwaiter<T> GetAwaiter() {
+        return this;
+    }
+
+    public void SetResult(T res) {
+        result=res;
+        isCompleted=true;
+        comp?.Invoke();
+    }
+
+    public T GetResult() {
+        return result;
+    }
+
+
+
+}
+
